fd70abbb3ea5eba833745f38a940b9dd485604cc
Stefano Onorati <stefanoonorati@hotmail.com>
2016-07-22 17:35:43 -0400

    Commit

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,11 @@
   $(function() {
     $('a[href*="#about"]').click(function() {
-     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     $('html, body').animate({
             scrollTop: target.offset().top
           }, 800);
           return false;
-        }
-      }
     });
   });
   
