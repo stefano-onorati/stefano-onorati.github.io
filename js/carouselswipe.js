9d72f4ff84b37df4ef611f17688f244c554ccde8
Stefano Onorati <stefanoonorati@hotmail.com>
2016-07-23 08:38:09 -0400

    Commit

---
+++ js/carouselswipe.js
@@ -0,0 +1,62 @@
+  	$(document).ready(function() {  
+   		$("#one").swiperight(function() {  
+      		$("#one").carousel('prev');  
+    		});  
+   		$("#one").swipeleft(function() {  
+   	  		$("#one").carousel('next');  
+   			});  
+	});  
+	  
+	$(document).ready(function() {  
+   		$("#two").swiperight(function() {  
+      		$("#two").carousel('prev');  
+    		});  
+   		$("#two").swipeleft(function() {  
+   	  		$("#two").carousel('next');  
+   			});  
+	});  
+	 
+	$(document).ready(function() {  
+   		$("#three").swiperight(function() {  
+      		$("#three").carousel('prev');  
+    		});  
+   		$("#three").swipeleft(function() {  
+   	  		$("#three").carousel('next');  
+   			});  
+	});  
+	 
+	$(document).ready(function() {  
+   		$("#four").swiperight(function() {  
+      		$("#four").carousel('prev');  
+    		});  
+   		$("#four").swipeleft(function() {  
+   	  		$("#four").carousel('next');  
+   			});  
+	});  
+	  
+	$(document).ready(function() {  
+   		$("#five").swiperight(function() {  
+      		$("#five").carousel('prev');  
+    		});  
+   		$("#five").swipeleft(function() {  
+   	  		$("#five").carousel('next');  
+   			});  
+	});  
+	  
+	$(document).ready(function() {  
+   		$("#six").swiperight(function() {  
+      		$("#six").carousel('prev');  
+    		});  
+   		$("#six").swipeleft(function() {  
+   	  		$("#six").carousel('next');  
+   			});  
+	});  
+	 
+	$(document).ready(function() {  
+   		$("#seven").swiperight(function() {  
+      		$("#seven").carousel('prev');  
+    		});  
+   		$("#seven").swipeleft(function() {  
+   	  		$("#seven").carousel('next');  
+   			});  
+	});  
